921ef7782b24007c582da610566dec63654c665c
GarouxE <133769796+GarouxE@users.noreply.github.com>
2025-12-31 18:27:40 +0100

    Update negamaxid.pl

--- algorithms/negamaxid.pl
+++ algorithms/negamaxid.pl
@@ -1,21 +1,21 @@
 :- module(negamax_id, [resolve/3]).
 :- use_module(negamax_tt).
 
-% We want high depth because ID allows it
+% On veut une grande profondueur parceque l'ID le permet
 final_depth(8). 
 
 resolve(Board, Token, Scores) :-
-    % 1. Clean memory
+    % 1. On nettoie la mémoire
     negamax_tt:clean_tt,
     final_depth(Max),
     Limit is Max - 1,
     
-    % 2. Loop depths 1 to Max-1 to warm up the cache
+    % 2. On boucle la profonduer entre 1 et Max - 1 pour le cache 
     forall(between(1, Limit, D),
            (
-            % We discard the scores of shallow searches, we only want the TT side-effects
+            % Nous ignorons les scores des recherches peu profondes. Nous ne conservons que les effets de bord de la table de transposition (TT)
             negamax_tt:solve_internal(Board, Token, D, _)
            )),
            
-    % 3. Final Search
+    % 3. Recherche finale
     negamax_tt:solve_internal(Board, Token, Max, Scores).
